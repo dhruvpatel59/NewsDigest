876641d6d815bb1206480fb153231d204f7a5fa1
Dhruv Patel <199948705+dhruvpatel59@users.noreply.github.com>
2026-03-11 19:32:40 +0530

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Dhruv Patel on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
